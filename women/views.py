36fff1fd864bc977e90c9241d759bc402f111d18
Nurbek <nurbek.malikov@nu.edu.kz>
2023-04-17 06:23:50 +0600

    Second Commit   + adds WomenSerializer

--- women/views.py
+++ women/views.py
@@ -1,3 +1,10 @@
+from rest_framework import generics
 from django.shortcuts import render
+from .models import Women
+from .serializer import WomenSerializer
+
 
 # Create your views here.
+class WomenAPIView(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
