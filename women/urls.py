b8b5798973a8a3dbde6201fce6ee178015a3a51f
Nurbek <nurbek.malikov@nu.edu.kz>
2023-04-20 03:57:28 +0600

    9th Commit   + adds router

--- women/urls.py
+++ women/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from . import views
 
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'women', views.WomenViewSet)
+
 urlpatterns = [
-    path("api/v1/womenlist/", views.WomenViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path("api/v1/womenlist/<int:pk>/", views.WomenViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path("api/v1/", include(router.urls)),
+    # path("api/v1/womenlist/", views.WomenViewSet.as_view({'get': 'list', 'post': 'create'})),
+    # path("api/v1/womenlist/<int:pk>/", views.WomenViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 ]
