509e57fc82a85b96dc8f2ea03f4350edbd4d25b8
Nurbek <nurbek.malikov@nu.edu.kz>
2023-04-19 19:15:15 +0600

    Forth Commit   + adds create and update method in WomenSerializer   + updates WomenAPIView post function (adds post and put)

--- women/urls.py
+++ women/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("api/v1/womenlist/", views.WomenAPIView.as_view()),
-    path("api/v1/womenlist/<int:pk/>", views.WomenAPIView.as_view()),
+    path("api/v1/womenlist/<int:pk>/", views.WomenAPIView.as_view()),
 ]
