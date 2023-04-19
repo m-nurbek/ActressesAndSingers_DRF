1fca5aa13a1df9576286c139e0e8f060ccb56249
Nurbek <nurbek.malikov@nu.edu.kz>
2023-04-20 03:13:54 +0600

    7th Commit   + simplifies the functionality

--- women/views.py
+++ women/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Women
 from .serializer import WomenSerializer
